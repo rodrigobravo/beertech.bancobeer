0c8936bdc55b13865ccde863545a6439dd94fc0f
fabricio-palhavam <fabricio.palhavam@ab-inbev.com>
2020-10-09 21:44:19 -0300

    BB-2 - Initial commit for consumer

---
+++ banco-beer-consumer/src/main/java/com/beertecth/bancobeerconsumer/BancoBeerConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.beertecth.bancobeerconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoBeerConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoBeerConsumerApplication.class, args);
+	}
+
+}
---
+++ banco-beer-consumer/src/main/java/com/beertecth/bancobeerconsumer/config/RabbitConfig.java
@@ -0,0 +1,10 @@
+package com.beertecth.bancobeerconsumer.config;
+
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableRabbit
+public class RabbitConfig {
+
+}
