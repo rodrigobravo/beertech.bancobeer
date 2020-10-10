f0a228a41cbe30281ac0b63c45c757fb60e95926
Rodrigo Bravo <bravo.rodrigo@gmail.com>
2020-10-09 22:46:37 -0300

    implementacao do endpoint /operacao

--- banco-beer-api/src/main/java/com/beertech/banco/controller/RestController.java
+++ banco-beer-api/src/main/java/com/beertech/banco/controller/RestController.java
@@ -1,20 +1,32 @@
 package com.beertech.banco.controller;
 
 
+import com.beertech.banco.entity.ContaCorrente;
+import com.beertech.banco.entity.TipoOperacao;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.math.BigDecimal;
 
 @Controller
 public class RestController {
 
-    @PutMapping(value = "/operacao/{tipo}/{valor}")
-    public void putDataOperacao(String tipo, Long valor){
-
+    @PutMapping(value = "/operacao")
+    public ContaCorrente putDataOperacao(@RequestParam(value = "tipo")TipoOperacao tipo,
+                                         @RequestParam(value = "valor")BigDecimal valor){
+    return new ContaCorrente(valor, tipo);
     }
 
 
     @PutMapping(value = "/saldo/")
     public void putDataSaldo(){
+
+    }
+
+    @GetMapping(value = "/saldo/")
+    public void getDataSaldo(){
 
     }
 
